30000d1807f06a871e2913072681a1a3217199c6
Alexander Krasnukhin <the.malkolm@gmail.com>
2016-08-22 09:25:34 +0200

    Add ID to tasks & taskGroups in reducer.
    This is somehow less error prone than adding ID in view components
    and follows redux pattern better.

--- src/reducers/job.js
+++ src/reducers/job.js
@@ -3,7 +3,16 @@ import { FETCHED_JOB, FETCHED_JOBS } from '../sagas/event';
 export function JobInfoReducer(state = { TaskGroups: [] }, action) {
     switch (action.type) {
         case FETCHED_JOB:
-            return action.payload
+            let job = action.payload;
+            job.TaskGroups.forEach(g => {
+                g.ID = job.ID + '.' + g.Name
+            });
+            job.TaskGroups.forEach(g => {
+                g.Tasks.forEach(t => {
+                    t.ID = g.ID + '.' + t.Name
+                })
+            });
+            return job;
         default:
     }
     return state
