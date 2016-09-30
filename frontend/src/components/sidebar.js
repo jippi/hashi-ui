6c37b56230a786d5984eacfa01c3ded7ff0f3d65
Christian Winther <cw@bownty.com>
2016-09-30 10:39:08 +0200

    use startsWith() instead of indexOf for sidebar

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -11,37 +11,37 @@ const Sidebar = ({ location }) => {
                     </Link>
                 </div>
                 <ul className="nav">
-                    <li className={location.pathname.indexOf('/cluster') === 0 ? 'active' : ''}>
+                    <li className={location.pathname.startsWith('/cluster') ? 'active' : ''}>
                         <Link to={{ pathname: '/cluster' }}>
                             <i className="pe-7s-cloud" />
                             <p>Cluster</p>
                         </Link>
                     </li>
-                    <li className={location.pathname.indexOf('/jobs') === 0 ? 'active' : ''}>
+                    <li className={location.pathname.startsWith('/jobs') ? 'active' : ''}>
                         <Link to={{ pathname: '/jobs' }}>
                             <i className="pe-7s-copy-file" />
                             <p>Jobs</p>
                         </Link>
                     </li>
-                    <li className={location.pathname.indexOf('/allocations') === 0 ? 'active' : ''}>
+                    <li className={location.pathname.startsWith('/allocations') ? 'active' : ''}>
                         <Link to={{ pathname: '/allocations' }}>
                             <i className="pe-7s-graph" />
                             <p>Allocations</p>
                         </Link>
                     </li>
-                    <li className={location.pathname.indexOf('/evaluations') === 0 ? 'active' : ''}>
+                    <li className={location.pathname.startsWith('/evaluations') ? 'active' : ''}>
                         <Link to={{ pathname: '/evaluations' }}>
                             <i className="pe-7s-gleam" />
                             <p>Evaluations</p>
                         </Link>
                     </li>
-                    <li className={location.pathname.indexOf('/members') === 0 ? 'active' : ''}>
+                    <li className={location.pathname.startsWith('/members') ? 'active' : ''}>
                         <Link to={{ pathname: '/members' }}>
                             <i className="pe-7s-share" />
                             <p>Members</p>
                         </Link>
                     </li>
-                    <li className={location.pathname.indexOf('/nodes') === 0 ? 'active' : ''}>
+                    <li className={location.pathname.startsWith('/nodes') ? 'active' : ''}>
                         <Link to={{ pathname: '/nodes' }}>
                             <i className="pe-7s-keypad" />
                             <p>Nodes</p>
