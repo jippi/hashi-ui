529a8f5a48d9f3ac3d8541c82770c544f9be5135
Christian Winther <cw@bownty.com>
2016-10-16 11:37:23 +0200

    display client meta and attributes

--- frontend/src/components/client/info.js
+++ frontend/src/components/client/info.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import MetaDisplay from '../meta';
 
 const nodeProps = [
     'ID',
@@ -13,16 +14,30 @@ const nodeProps = [
 
 const ClientInfo = ({ node }) =>
   <div className="tab-pane active">
-    <div className="content">
-      <legend>Client Properties</legend>
-      <dl className="dl-horizontal">
-        { nodeProps.map(nodeProp =>
-          <div key={ nodeProp }>
-            <dt>{ nodeProp }</dt>
-            <dd>{ node[nodeProp] }</dd>
-          </div>
-        )}
-      </dl>
+    <div className="row">
+      <div className="col-lg-6 col-md-6 col-sm-6 col-sx-6">
+        <legend>Client Properties</legend>
+        <dl className="dl-horizontal">
+          { nodeProps.map(nodeProp =>
+            <div key={ nodeProp }>
+              <dt>{ nodeProp }</dt>
+              <dd>{ node[nodeProp] }</dd>
+            </div>
+          )}
+        </dl>
+      </div>
+      <div className="col-lg-6 col-md-6 col-sm-6 col-sx-6">
+        <legend>Meta Properties</legend>
+        <MetaDisplay dtWithClass="wide" metaBag={ node.Meta } />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-lg-12 col-md-12 col-sm-12 col-sx-12">
+        <div className="content">
+          <legend>Client Attributes</legend>
+          <MetaDisplay dtWithClass="wide" metaBag={ node.Attributes } />
+        </div>
+      </div>
     </div>
   </div>;
 
