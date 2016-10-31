c7546167132402b19ae3eac19f0d2e934017143c
Christian Winther <cw@bownty.com>
2016-10-31 17:39:58 +0100

    even wider task group column

--- frontend/src/components/allocation/list.js
+++ frontend/src/components/allocation/list.js
@@ -188,7 +188,7 @@ class AllocationList extends Component {
                     <th width="40"></th>
                     <th width="120">ID</th>
                     { jobHeaderColumn(showJobColumn) }
-                    <th width="350">Task Group</th>
+                    <th width="300">Task Group</th>
                     <th width="120">Status</th>
                     { clientHeaderColumn(showClientColumn) }
                     <th width="120">Age</th>
