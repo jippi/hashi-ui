7a60cec795df96b2468f3f1baf6818a338be481a
Christian Winther <cw@bownty.com>
2016-12-15 20:51:00 +0100

    slighly refactor task groups and make them more prominent in the top menu

--- frontend/src/components/JobTaskGroups/JobTaskGroups.js
+++ frontend/src/components/JobTaskGroups/JobTaskGroups.js
@@ -2,59 +2,105 @@ import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 import { Card, CardTitle, CardText } from 'material-ui/Card'
-import { TableRow, TableRowColumn } from '../Table'
+import { Table, TableHeader, TableRow, TableHeaderColumn, TableBody, TableRowColumn } from '../Table'
 import JobLink from '../JobLink/JobLink'
-import TableHelper from '../TableHelper/TableHelper'
 import RawJson from '../RawJson/RawJson'
-import MetaPayload from '../MetaPayload/MetaPayload'
-
-const taskGroupHeaders = [
-  'ID',
-  'Name',
-  'Count',
-  'Meta',
-  'Restart Policy'
-]
+import JobTaskGroupActionScale from '../JobTaskGroupActionScale/JobTaskGroupActionScale'
+import JobTaskGroupActionStop from '../JobTaskGroupActionStop/JobTaskGroupActionStop'
+import ConstraintTable from '../ConstraintTable/ConstraintTable'
 
 const JobTaskGroups = ({ job, location }) => {
   const taskGroups = []
+  const tasks = {}
 
   job.TaskGroups.forEach((taskGroup) => {
     taskGroups.push(
       <TableRow key={ taskGroup.ID }>
-        <TableRowColumn><JobLink taskGroupId={ taskGroup.ID } jobId={ job.ID } /></TableRowColumn>
-        <TableRowColumn>{ taskGroup.Name }</TableRowColumn>
-        <TableRowColumn>{ taskGroup.Count }</TableRowColumn>
-        <TableRowColumn><MetaPayload metaBag={ taskGroup.Meta } asTooltip /></TableRowColumn>
-        <TableRowColumn>{ taskGroup.RestartPolicy.Mode }</TableRowColumn>
+        <TableRowColumn>
+          <JobLink taskGroupId={ taskGroup.ID } jobId={ job.ID }>{ taskGroup.Name }</JobLink>
+        </TableRowColumn>
+        <TableRowColumn style={{ textAlign: 'right' }}>{ taskGroup.Count }</TableRowColumn>
+        <TableRowColumn>
+          <JobTaskGroupActionScale job={ job } taskGroup={ taskGroup } />
+          <JobTaskGroupActionStop job={ job } taskGroup={ taskGroup } />
+        </TableRowColumn>
       </TableRow>
     )
+
+    taskGroup.Tasks.map((task) => {
+      tasks[taskGroup.ID] = tasks[taskGroup.ID] || []
+      tasks[taskGroup.ID].push(
+        <TableRow key={ task.ID }>
+          <TableRowColumn>
+            <JobLink jobId={ job.ID } taskGroupId={ taskGroup.ID } taskId={ task.ID } >
+              { task.Name }
+            </JobLink>
+          </TableRowColumn>
+          <TableRowColumn>{ task.Driver }</TableRowColumn>
+          <TableRowColumn>{ task.Resources.CPU }</TableRowColumn>
+          <TableRowColumn>{ task.Resources.MemoryMB }</TableRowColumn>
+          <TableRowColumn>{ task.Resources.DiskMB }</TableRowColumn>
+          <TableRowColumn>
+            <ConstraintTable idPrefix={ task.ID } asTooltip constraints={ task.Constraints } />
+          </TableRowColumn>
+        </TableRow>
+      )
+    })
   })
 
   let taskGroupId = location.query.taskGroupId
 
-    // Auto-select first task group if only one is available.
-  if (!taskGroupId && job.TaskGroups.length === 1) {
+  // Auto-select first task group if only one is available.
+  if (!taskGroupId && job.TaskGroups.length > 0) {
     taskGroupId = job.TaskGroups[0].ID
   }
 
   return (
     <Grid fluid style={{ padding: 0 }}>
       <Row>
-        <Col key='task-groups-pane' xs={ 12 } sm={ 12 } md={ 6 } lg={ 5 }>
+        <Col key='task-groups-pane' xs={ 12 } sm={ 12 } md={ 4 } lg={ 3 }>
           <Card>
             <CardTitle title='Task Groups' />
             <CardText>
-              { (taskGroups.length > 0)
-                ? <TableHelper headers={ taskGroupHeaders } body={ taskGroups } />
-                : null
-              }
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHeaderColumn key='name'>Name</TableHeaderColumn>
+                    <TableHeaderColumn key='count' style={{ width: 40, textAlign: 'right' }}>Count</TableHeaderColumn>
+                    <TableHeaderColumn key='actions' style={{ width: 50 }}>Actions</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  { taskGroups }
+                </TableBody>
+              </Table>
             </CardText>
           </Card>
         </Col>
-        <Col key='data-pane' xs={ 12 } sm={ 12 } md={ 6 } lg={ 7 }>
+        <Col key='data-pane' xs={ 12 } sm={ 12 } md={ 8 } lg={ 9 }>
           <Card>
-            <CardTitle title={ taskGroupId } />
+            <CardTitle title={ `Tasks in "${taskGroupId}"` } />
+            <CardText>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHeaderColumn key='name'>Name</TableHeaderColumn>
+                    <TableHeaderColumn key='driver' style={{ width: 80 }}>Driver</TableHeaderColumn>
+                    <TableHeaderColumn key='cpu' style={{ width: 80 }}>CPU</TableHeaderColumn>
+                    <TableHeaderColumn key='memory' style={{ width: 80 }}>Memory</TableHeaderColumn>
+                    <TableHeaderColumn key='disk' style={{ width: 80 }}>Disk</TableHeaderColumn>
+                    <TableHeaderColumn key='constraints' style={{ width: 80 }}>Constraints</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  { tasks[taskGroupId] }
+                </TableBody>
+              </Table>
+            </CardText>
+          </Card>
+
+          <Card style={{ marginTop: '1rem' }}>
+            <CardTitle title={ `Raw JSON for "${taskGroupId}"` } />
             <CardText>
               { job.TaskGroups
                   .filter(taskGroup => taskGroup.ID === taskGroupId)
