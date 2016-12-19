0e5a62412aee4c21dd4501b54ac6c24016b344e3
Christian Winther <cw@bownty.com>
2016-12-19 13:38:56 +0100

    fix regex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,9 @@ import { createHistory } from 'history'
 import { useRouterHistory } from 'react-router'
 
 const browserHistory = useRouterHistory(createHistory)({
-  basename: window['NOMAD_ENDPOINT']
+  // attempt to remove host+port from NOMAD_ENDPOINT
+  // long term this could be done by splitting on "/" and take chunk "1..."
+  basename: window['NOMAD_ENDPOINT'].replace(/^[-.a-zA-Z0-9]+:?[0-9]*/, '')
 })
 
 import { Provider } from 'react-redux'
