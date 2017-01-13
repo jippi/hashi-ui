33f24de0fdd16f3c02bad52cadd5042510308b0d
Christian Winther <cw@bownty.com>
2017-01-13 21:00:17 +0100

    [nomad] make breadcrumb links for single-view of allocation
    now you can direct link to both the job and task group the allocation belongs to

--- frontend/src/containers/allocation.js
+++ frontend/src/containers/allocation.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import AllocationTopbar from '../components/AllocationTopbar/AllocationTopbar'
+import JobLink from '../components/JobLink/JobLink'
 import { WATCH_ALLOC, UNWATCH_ALLOC } from '../sagas/event'
 
 class Allocation extends Component {
@@ -19,6 +20,15 @@ class Allocation extends Component {
     })
   }
 
+  getName() {
+    const name = this.props.allocation.Name
+
+    return name.substring(
+      name.indexOf("[") + 1,
+      name.indexOf("]")
+    )
+  }
+
   render () {
     if (this.props.allocation == null) {
       return null
@@ -29,7 +39,23 @@ class Allocation extends Component {
         <AllocationTopbar { ...this.props } />
 
         <div style={{ padding: 10, paddingBottom: 0 }}>
-          <h2>Allocation: { this.props.allocation.Name }</h2>
+          <h2>
+            Allocation:
+
+            &nbsp;
+
+            <JobLink jobId={ this.props.allocation.JobID } />
+
+            &nbsp; > &nbsp;
+
+            <JobLink jobId={ this.props.allocation.JobID } taskGroupId={ this.props.allocation.TaskGroupId }>
+              { this.props.allocation.TaskGroup }
+            </JobLink>
+
+            &nbsp; >
+
+            #{ this.getName() }
+          </h2>
 
           <br />
 
