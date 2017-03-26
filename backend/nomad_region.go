ef71ed8cd991edb1488b3cd42f6f47c051b2101a
Wintermute1 <conor.gannon.1@ucdconnect.ie>
2017-03-26 19:43:00 +0100

    remove elif

--- backend/nomad_region.go
+++ backend/nomad_region.go
@@ -248,7 +248,9 @@ func (n *NomadRegion) updateJob(job *api.Job) (*Action, error) {
 	if n.Config.NomadReadOnly {
 		logger.Errorf("Unable to run job: NomadReadOnly is set to true")
 		return &Action{Type: errorNotification, Payload: "The backend server is set to read-only"}, errors.New("Nomad is in read-only mode")
-	} else if defaultConfig.HideEnvData {
+	}
+
+	if defaultConfig.HideEnvData {
 		logger.Errorf("Unable to submit job: HideEnvData is set to true")
 		return &Action{Type: errorNotification, Payload: "HideEnv is set to true"}, errors.New("HideEnvData must be false to update")
 
