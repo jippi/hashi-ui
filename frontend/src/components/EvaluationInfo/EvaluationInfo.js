aba5743cb30fed74ff072ef68ef6f3c5b3b3935c
Jonathan Ballet <jballet@edgelab.ch>
2017-05-18 11:57:28 +0200

    Improve evaluation info tab
    * fix the panel title
    * add missing attributes (description is cool)
    * add a link towards the related job
    * add links between next/previous evaluation

--- frontend/src/components/EvaluationInfo/EvaluationInfo.js
+++ frontend/src/components/EvaluationInfo/EvaluationInfo.js
@@ -1,37 +1,58 @@
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 import { Card, CardTitle, CardText } from 'material-ui/Card'
+import MetaPayload from '../MetaPayload/MetaPayload'
+import EvaluationLink from '../EvaluationLink/EvaluationLink'
+import JobLink from '../JobLink/JobLink'
 
 const evaluationProps = [
   'ID',
   'Status',
+  'StatusDescription',
+  'NextEval',
+  'PreviousEval',
   'Priority',
   'Type',
   'JobID',
   'TriggeredBy'
 ]
 
-const EvaluationInfo = ({ evaluation }) =>
-  <Grid fluid style={{ padding: 0 }}>
-    <Row>
-      <Col key='properties-pane' xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 }>
-        <Card>
-          <CardTitle title='Client Properties' />
-          <CardText>
-            <dl className='dl-horizontal'>
-              { evaluationProps.map(evalProp =>
-                <div key={ evalProp }>
-                  <dt>{ evalProp }</dt>
-                  <dd>{ evaluation[evalProp] }</dd>
-                </div>
-              )}
-            </dl>
-          </CardText>
-        </Card>
-      </Col>
-    </Row>
-  </Grid>
+class EvaluationInfo extends Component {
+  render () {
+    const evaluation = this.props.evaluation
+    const evalValues = {}
+
+    evaluationProps.map((evalProp) => {
+      evalValues[evalProp] = evaluation[evalProp] ? evaluation[evalProp] : '-'
+      return null
+    })
+
+    evalValues.JobID = <JobLink jobId={ evaluation.JobID } />
+
+    if (evaluation.PreviousEval) {
+      evalValues.PreviousEval = <EvaluationLink evaluationId={ evaluation.PreviousEval } />
+    }
+    if (evaluation.NextEval) {
+      evalValues.NextEval = <EvaluationLink evaluationId={ evaluation.NextEval } />
+    }
+
+    return (
+      <Grid fluid style={{ padding: 0 }}>
+        <Row>
+          <Col key='properties-pane' xs={ 12 } sm={ 12 } md={ 12 } lg={ 12 }>
+            <Card>
+              <CardTitle title='Evaluation Properties' />
+              <CardText>
+                <MetaPayload metaBag={ evalValues } sortKeys={ false } />
+              </CardText>
+            </Card>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
 
 
 function mapStateToProps ({ evaluation }) {
