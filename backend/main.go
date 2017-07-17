78ed5379d5eaeb532b896398a3b4381c8c5f50f8
Christian Winther <jippignu@gmail.com>
2017-07-17 14:45:18 +0200

    proper cache js bundle

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/newrelic/go-agent"
@@ -212,7 +211,12 @@ func main() {
 		if bs, assetErr := myAssetFS.Open(responseFile); err != nil {
 			logger.Errorf("%s: %s", responseFile, assetErr)
 		} else {
-			http.ServeContent(w, r, responseFile[1:], time.Now(), bs)
+			stat, err := bs.Stat()
+			if err != nil {
+				logger.Errorf("Failed to stat %s: %s", responseFile, err)
+			} else {
+				http.ServeContent(w, r, responseFile[1:], stat.ModTime(), bs)
+			}
 		}
 	})
 
