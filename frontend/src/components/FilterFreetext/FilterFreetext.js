a5e0288cec910dacd2df1d2dfbc73b469d0b128b
Christian Winther <jippignu@gmail.com>
2017-09-21 19:24:39 +0200

    focus on name when loading jobs

--- frontend/src/components/FilterFreetext/FilterFreetext.js
+++ frontend/src/components/FilterFreetext/FilterFreetext.js
@@ -1,32 +1,44 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
 import TextField from "material-ui/TextField"
 import { withRouter } from "react-router"
 
-const FilterFreetext = ({ label, query, location, router }) => {
-  const q = location.query || {}
+class FilterFreetext extends Component {
+  componentDidMount() {
+    if (this.props.focusOnLoad) {
+      this.nameInput.focus()
+    }
+  }
 
-  return (
-    <TextField
-      floatingLabelText={label}
-      value={q[query] || ""}
-      onChange={(proxy, value) => {
-        q[query] = value
+  render() {
+    const q = this.props.location.query || {}
 
-        router.push({
-          pathname: location.pathname,
-          query: q
-        })
-      }}
-    />
-  )
+    return (
+      <TextField
+        ref={a => {
+          this.nameInput = a
+        }}
+        floatingLabelText={this.props.label}
+        value={q[this.props.query] || ""}
+        onChange={(proxy, value) => {
+          q[this.props.query] = value
+
+          router.push({
+            pathname: this.props.location.pathname,
+            query: q
+          })
+        }}
+      />
+    )
+  }
 }
 
 FilterFreetext.propTypes = {
   location: PropTypes.object.isRequired,
   router: PropTypes.object.isRequired,
   query: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired
+  label: PropTypes.string.isRequired,
+  focusOnLoad: PropTypes.bool
 }
 
 export default withRouter(FilterFreetext)
