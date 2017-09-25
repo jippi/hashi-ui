780139176b78c934d9d5669dfb47be56b2110d09
Christian Winther <jippignu@gmail.com>
2017-09-25 08:22:44 +0200

    only count used resources for allocations that should/are running

--- backend/nomad/nodes/stats.go
+++ backend/nomad/nodes/stats.go
@@ -74,11 +74,13 @@ func (w *stats) work(client *api.Client, send chan *structs.Action, subscribeCh 
 	taksResult.MemoryAllocated = 0
 
 	for _, allocation := range allocations {
-		if allocation.ClientStatus == "running" {
-			for _, resources := range allocation.TaskResources {
-				taksResult.MemoryAllocated += int64(*resources.MemoryMB)
-				taksResult.CPUAllocatedMHz += int64(*resources.CPU)
-			}
+		if allocation.DesiredStatus != "run" {
+			continue
+		}
+
+		for _, resources := range allocation.TaskResources {
+			taksResult.MemoryAllocated += int64(*resources.MemoryMB)
+			taksResult.CPUAllocatedMHz += int64(*resources.CPU)
 		}
 	}
 
